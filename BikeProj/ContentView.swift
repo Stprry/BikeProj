37702525e681c303d5ea84ce9f643ff250fd635f
Stprry <stprry96@gmail.com>
2020-02-07 15:03:54 +0000

    Initial Commit

---
+++ BikeProj/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BikeProj
+//
+//  Created by Sam Perry on 07/02/2020.
+//  Copyright © 2020 Sam Perry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
